c3f58f6dbf801733069fa6220f6e1acbe4bee08f
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2024-03-22 20:51:36 +0700

    Add a test db and some test codes

--- profile-crud/app.js
+++ profile-crud/app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//TUAN DB TEST
+var db = require("./database")
+var md5 = require("md5")
+
+var bodyParser = require("body-parser");
+
+//END
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +29,111 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//TUAN TEST
+
+app.get("/api/users", (req, res, next) => {
+  var sql = "select * from user"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+app.get("/api/user/:id", (req, res, next) => {
+  var sql = "select * from user where id = ?"
+  var params = [req.params.id]
+  db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":row
+      })
+    });
+});
+
+app.post("/api/user/", (req, res, next) => {
+  var errors=[]
+  if (!req.body.password){
+      errors.push("No password specified");
+  }
+  if (!req.body.email){
+      errors.push("No email specified");
+  }
+  if (errors.length){
+      res.status(400).json({"error":errors.join(",")});
+      return;
+  }
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : md5(req.body.password)
+  }
+  var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+  var params =[data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+      if (err){
+          res.status(400).json({"error": err.message})
+          return;
+      }
+      res.json({
+          "message": "success",
+          "data": data,
+          "id" : this.lastID
+      })
+  });
+})
+
+app.patch("/api/user/:id", (req, res, next) => {
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : req.body.password ? md5(req.body.password) : null
+  }
+  db.run(
+      `UPDATE user set 
+         name = COALESCE(?,name), 
+         email = COALESCE(?,email), 
+         password = COALESCE(?,password) 
+         WHERE id = ?`,
+      [data.name, data.email, data.password, req.params.id],
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({
+              message: "success",
+              data: data,
+              changes: this.changes
+          })
+  });
+})
+
+app.delete("/api/user/:id", (req, res, next) => {
+  db.run(
+      'DELETE FROM user WHERE id = ?',
+      req.params.id,
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({"message":"deleted", changes: this.changes})
+  });
+})
+
+//END TUAN TEST
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
