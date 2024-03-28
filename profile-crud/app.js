191f0e6c921816477a2fa64a0f8d4adf7ddb2c44
imadvm <contact.imadvm@gmail.com>
2024-03-29 01:33:41 +0700

    install jwt

--- profile-crud/app.js
+++ profile-crud/app.js
@@ -1,154 +1,156 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 //TUAN DB TEST
-var db = require("./database")
-var md5 = require("md5")
+var db = require("./database");
+var md5 = require("md5");
 
 var bodyParser = require("body-parser");
 
 //END
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 //TUAN TEST
 
 app.get("/api/users", (req, res, next) => {
-  var sql = "select * from user"
-  var params = []
+  var sql = "select * from user";
+  var params = [];
   db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(400).json({"error":err.message});
-        return;
-      }
-      res.json({
-          "message":"success",
-          "data":rows
-      })
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
     });
+  });
 });
 
 app.get("/api/user/:id", (req, res, next) => {
-  var sql = "select * from user where id = ?"
-  var params = [req.params.id]
+  var sql = "select * from user where id = ?";
+  var params = [req.params.id];
   db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({"error":err.message});
-        return;
-      }
-      res.json({
-          "message":"success",
-          "data":row
-      })
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
     });
+  });
 });
 
 app.post("/api/user/", (req, res, next) => {
-  var errors=[]
-  if (!req.body.password){
-      errors.push("No password specified");
+  var errors = [];
+  if (!req.body.password) {
+    errors.push("No password specified");
   }
-  if (!req.body.email){
-      errors.push("No email specified");
+  if (!req.body.email) {
+    errors.push("No email specified");
   }
-  if (errors.length){
-      res.status(400).json({"error":errors.join(",")});
-      return;
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
   }
   var data = {
-      name: req.body.name,
-      email: req.body.email,
-      password : md5(req.body.password)
-  }
-  var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-  var params =[data.name, data.email, data.password]
+    name: req.body.name,
+    email: req.body.email,
+    password: md5(req.body.password),
+  };
+  var sql = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
+  var params = [data.name, data.email, data.password];
   db.run(sql, params, function (err, result) {
-      if (err){
-          res.status(400).json({"error": err.message})
-          return;
-      }
-      res.json({
-          "message": "success",
-          "data": data,
-          "id" : this.lastID
-      })
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data,
+      id: this.lastID,
+    });
   });
-})
+});
 
 app.patch("/api/user/:id", (req, res, next) => {
   var data = {
-      name: req.body.name,
-      email: req.body.email,
-      password : req.body.password ? md5(req.body.password) : null
-  }
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password ? md5(req.body.password) : null,
+  };
   db.run(
-      `UPDATE user set 
+    `UPDATE user set 
          name = COALESCE(?,name), 
          email = COALESCE(?,email), 
          password = COALESCE(?,password) 
          WHERE id = ?`,
-      [data.name, data.email, data.password, req.params.id],
-      function (err, result) {
-          if (err){
-              res.status(400).json({"error": res.message})
-              return;
-          }
-          res.json({
-              message: "success",
-              data: data,
-              changes: this.changes
-          })
-  });
-})
+    [data.name, data.email, data.password, req.params.id],
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+      res.json({
+        message: "success",
+        data: data,
+        changes: this.changes,
+      });
+    }
+  );
+});
 
 app.delete("/api/user/:id", (req, res, next) => {
   db.run(
-      'DELETE FROM user WHERE id = ?',
-      req.params.id,
-      function (err, result) {
-          if (err){
-              res.status(400).json({"error": res.message})
-              return;
-          }
-          res.json({"message":"deleted", changes: this.changes})
-  });
-})
+    "DELETE FROM user WHERE id = ?",
+    req.params.id,
+    function (err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+      res.json({ message: "deleted", changes: this.changes });
+    }
+  );
+});
 
 //END TUAN TEST
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
